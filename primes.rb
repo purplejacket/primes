cf1f896f2093140a34a9027fbe6dbb70f8c5425b
Kai Middleton <kai.middleton@gmail.com>
2014-12-03 17:55:46 -0800

    added simple size test

--- primes.rb
+++ primes.rb
@@ -2,7 +2,12 @@
 # for n >=55 P(n) < n * (log n  +  log log n  + 2)
 
 class Primes
-  def initialize n
-    nil
+  @first_54_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]
+  attr_reader :size
+
+  def initialize size
+    @size = size
   end
+
+
 end
--- primes_spec.rb
+++ primes_spec.rb
@@ -1,6 +1,10 @@
 require './primes.rb'
 
 describe Primes do
+  it "tells you how big it is" do
+    primes = Primes.new(33)
+    expect(primes.size).to eq(33)
+  end
   it "Works trivially" do
     primes = Primes.new(1)
     expect(primes).to eq([2])
