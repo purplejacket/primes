16a1c343232b9b3b36a597a4e8b30a91d0a24fdd
Kai Middleton <kai.middleton@gmail.com>
2014-12-03 20:01:18 -0800

    now passing tests for bounds on size of the nth prime

--- primes.rb
+++ primes.rb
@@ -1,13 +1,23 @@
 # Notes
 # for n >=55 P(n) < n * (log n  +  log log n  + 2)
+# Rosser, J.B. 1941
+#    Explicit bounds for some functions of prime numbers. American Journal of Math., 63, 211--232.
 
 class Primes
-  @first_54_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]
-  attr_reader :size
 
-  def initialize size
-    @size = size
+  attr_reader :size, :bound, :all
+
+  def initialize n
+    @first_54_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]
+    @size = n
+    @bound = a_bound_for_the_nth_prime(n)
   end
 
-
+  def a_bound_for_the_nth_prime(n)
+    if n <= @first_54_primes.length
+      @first_54_primes[n-1]
+    else
+      (n * (Math.log(n) + Math.log(Math.log(n)) + 2)).ceil
+    end
+  end
 end
--- primes_spec.rb
+++ primes_spec.rb
@@ -5,6 +5,12 @@ describe Primes do
     primes = Primes.new(33)
     expect(primes.size).to eq(33)
   end
+  it "Given n, it tells you an upper bound for P(n)" do
+    primes = Primes.new(55)
+    expect(primes.bound).to eq(407)
+    primes = Primes.new(10)
+    expect(primes.bound).to eq(29)
+  end
   it "Works trivially" do
     primes = Primes.new(1)
     expect(primes).to eq([2])
